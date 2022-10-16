b0b2989c56e0024962748dcb5836dde2b2659cd4
k.can_mersin <73068663+kcanmersin@users.noreply.github.com>
2022-10-16 20:31:39 +0300

    Time: 3 ms (51.01%), Space: 6.3 MB (50.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:  
+bool isValid(string s) {
+    stack<char> stck;
+    for(char ch : s){
+        if(ch == '('|| ch == '{' || ch == '['){
+            stck.push(ch);
+        }else
+        {
+            if(stck.empty()) return false;
+            if(ch == ')' && stck.top() != '(')
+                return false;
+            if(ch == '}' && stck.top() != '{')
+                return false;
+            if(ch == ']' && stck.top() != '[')
+                return false;
+            stck.pop();
+        }
+    }
+    return stck.empty();
+}
+};
