e4a9a3cd382be7588b6e47d019d1d7b20530dd32
k.can_mersin <73068663+kcanmersin@users.noreply.github.com>
2022-10-19 22:45:46 +0300

    Time: 102 ms (93.05%), Space: 46.6 MB (80.80%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int a=0;a<nums.size()-1;a++)
+        {
+            //printf("%d ",nums[a]);
+                if(nums[a]==nums[a+1])
+                {
+                    return true;
+                }
+            
+        }
+        return false;
+    }
+};
